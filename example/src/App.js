8a04ba1976408a7a2442c2ca6e6321dad6675efd
Thiago Canto <t.canto@manusis4.com>
2020-11-05 21:35:39 -0300

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'a-simple-carousel'
+import 'a-simple-carousel/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
